cebdfd2b2551828072b5769d1a5d0128df5de833
Michael Bullington <michael@bullington.xyz>
2021-03-11 23:05:35 -0500

    work on building for multiple platforms, running profile in Chrome

--- wasmc.js
+++ wasmc.js
@@ -1,22 +1,50 @@
+const DEBUG = true
+
 const FLAGS = ['-std=c++11']
+if (!DEBUG) {
+    FLAGS.push('-O3')
+} else {
+    FLAGS.push('-O0')
+    FLAGS.push('--profiling')
+}
 
 // Add include for submodules.
 //
 // IMPORTANT: For wasmc, the __dirname is mounted to /src for Docker.
-const CFLAGS = ['-O3', '-I/src/third_party/geometry.hpp/include']
+const CFLAGS = ['-I/src/third_party/geometry.hpp/include']
 
 const LFLAGS = ['-lm']
 
 // Reference:
 // https://github.com/rsms/js-wasmc
-module({
-    name: 'astro',
-    out: 'dist/astro.js',
+
+const common = {
     jsentry: 'src/index.js',
     sources: ['src/*.cpp', 'third_party/martinez/*.cpp'],
     cflags: [...FLAGS, ...CFLAGS],
     lflags: [...FLAGS, ...LFLAGS],
-    // Node.js only options.
+}
+
+// Available as window.astro
+module({
+    ...common,
+    name: 'astro',
+    out: 'dist/astro.js',
+})
+
+// ES module.
+module({
+    ...common,
+    name: 'astro-es',
+    out: 'dist/astro.es.js',
+    format: 'es',
+})
+
+// Node.js
+module({
+    ...common,
+    name: 'astro-node',
+    out: 'dist/astro.node.js',
     target: 'node',
     embed: true,
 })
